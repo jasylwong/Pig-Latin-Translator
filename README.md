12cb8d0045a81abca231eb508a3d1645e030df64
jasylwong <jasylwong@gmail.com>
2020-01-17 11:33:21 +0000

    Updated README.md

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
+#Pig Latin translator#
+
 A translator to help you translate English into Pig Latin!
 
 Assumptions:
 
 - Input may consist of alphabetic characters only (A-Z, a-z, numbers and spaces)
+- All punctuation, numerals, symbols and whitespace are not modified
+
 
 Features I am in the process of adding:
 
-- Punctuation marks - multiple sentences
 - Hyphenated words are treated as two words
-- All punctuation, numerals, symbols and whitespace are not modified
+- Punctuation marks - multiple sentences
--- pig_latin_translator.rb
+++ pig_latin_translator.rb
@@ -1,37 +1,63 @@
 # Pig Latin Translator
 
-
+require 'pry'
 # Assumptions:
     # Words may consist of alphabetic characters only (A-Z and a-z)
 
 def sentenceTo(sentence)
-    words = sentence.split(" ")
+    words = words = sentence.split(%r{(\b[^\s]+\b)}).reject{|s| s==""}.reject{|s| s==" " }
+
     new_sentence = []
     for word in words
         new_sentence.push(singleWordTo(word))
     end
     puts "----------------------------"
     puts "\"#{sentence}\" in pig latin is: "
-    puts new_sentence.join(" ")
+
+    final_sentence = ""
+
+    new_sentence.each_with_index do |word, index|
+        if (['!', '?', '.', ','].include?(word))
+            final_sentence << word
+        elsif index == 0
+            final_sentence << word
+        else
+            final_sentence << " #{word}"
+        end
+    end
+    puts final_sentence
     puts "----------------------------"
     translateMore
 end
 
 def singleWordTo(word)
+
     if %w(a e i o u).include?(word[0].downcase)
+
         word == word.upcase ? word + "WAY" : word + "way"
     elsif "qu".include?(word[0..1].downcase)
-        word_temp = word[2..-1] + word[0..1]
+
+        word_temp =  takes_first_shoves_in_back(word)
         word = (word == word.capitalize ? word_temp.capitalize : word_temp)
         word == word.upcase ? word + "AY" : word + "ay"
     else
+
         word_temp = word[1..-1] + word[0]
-        until %w(a e i o u y).include?(word_temp[0].downcase)
-            word_temp = word_temp[1..-1] + word_temp[0]
+
+        if !(['!', '?', '.', ','].include?(word_temp))
+            until %w(a e i o u y).include?(word_temp[0].downcase)
+                word_temp = word_temp[1..-1] + word_temp[0]
+            end
+            word = (word == word.capitalize ? word_temp.capitalize : word_temp)
+            word == word.upcase ? word + "AY" : word + "ay"
+        else
+            word_temp
         end
-        word = (word == word.capitalize ? word_temp.capitalize : word_temp)
-        word == word.upcase ? word + "AY" : word + "ay"
     end
+end
+
+def takes_first_shoves_in_back(word)
+    word[2..-1] + word[0..1]
 end
 
 def translateMore
